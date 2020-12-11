11d015abeb7a98dc4ff650572d7a995f1b1aa428
James Nakano <confuzedskull@mail.com>
2020-12-10 21:47:39 -0800

    first commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojurest "0.0.1"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "1.0.0"]
+                 [http-kit "2.5.0"]]
+  :main clojurest.core)
---
+++ src/clojurest/core.clj
@@ -0,0 +1,46 @@
+(ns clojurest.core
+  (:require [clojure.data.json :as json]
+            [org.httpkit.server :as httpkit]))
+
+(defn respond [& body]
+  (prn "response" body)
+  {:status 200
+   :body body
+   :headers {"Access-Control-Allow-Origin"
+             "*"
+             "Access-Control-Allow-Headers"
+             "Content-Type"}})
+
+(defn test-service []
+  (prn "testing"))
+
+(defn secret-service [method body]
+  (case method
+    :get
+    (respond (slurp "resources/secret"))
+    :put
+    (do (spit "resources/secret" body)
+        (respond))
+    {:status 405}))
+
+(defn handler [request]
+  (let [method (:request-method request)
+        body (if (some? (:body request))
+               (-> (:body request)
+                   slurp
+                   json/read-str)
+               "")
+        uri (:uri request)]
+    (prn "request" method body uri)
+    (case method
+      :options
+      (respond)
+      (case uri
+        "/secret"
+        (secret-service method body)
+        "/test"
+        (test-service)
+        (respond "Hello, World!")))))
+
+(defn -main [& args]
+  (httpkit/run-server handler))
