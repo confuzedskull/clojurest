8a5f15add22ee9c529db3cf25b10a99b4d6fa51f
James Nakano <confuzedskull@mail.com>
2021-08-22 23:33:19 -0700

    added another header to support OPTIONS requests

--- src/clojurest/core.clj
+++ src/clojurest/core.clj
@@ -9,6 +9,8 @@
    :body body
    :headers {"Access-Control-Allow-Origin"
              "*"
+             "Access-Control-Allow-Methods"
+             "*"
              "Access-Control-Allow-Headers"
              "*"}})
 
