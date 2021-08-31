a37d0c8daef608e3241ea9fa75f1b2b1c6fe0d1c
James Nakano <confuzedskull@mail.com>
2021-08-30 21:15:05 -0700

    incremented version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurest "0.0.1"
+(defproject clojurest "0.0.2"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
                  [http-kit "2.5.0"]]
